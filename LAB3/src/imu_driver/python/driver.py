5fc78882fef4f6498b46431340804a7ee262c6e2
thumattisaravanan-n <thumattisaravanan.n@northeastern.edu>
2023-11-20 12:57:05 -0500

    lab3

--- LAB3/src/imu_driver/python/driver.py
+++ LAB3/src/imu_driver/python/driver.py
@@ -1,112 +1,105 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/python3
 
-import sys
 import rospy
 import serial
+from sensor_msgs.msg import Imu, MagneticField
+from geometry_msgs.msg import Vector3
 from imu_driver.msg import imu_msg
-from imu_driver.srv import EulerToQuaternion, EulerToQuaternionRequest, EulerToQuaternionResponse
-import numpy as np
+from imu_driver.srv import *
+import math
 
-def toquat(r,p,y):
-	qx = np.sin(r/2) * np.cos(p/2) * np.cos(y/2) - np.cos(r/2) * np.sin(p/2) * np.sin(y/2)
-	qy = np.cos(r/2) * np.sin(p/2) * np.cos(y/2) + np.sin(r/2) * np.cos(p/2) * np.sin(y/2)
-	qz = np.cos(r/2) * np.cos(p/2) * np.sin(y/2) - np.sin(r/2) * np.sin(p/2) * np.cos(y/2)
-	qw = np.cos(r/2) * np.cos(p/2) * np.cos(y/2) + np.sin(r/2) * np.sin(p/2) * np.sin(y/2)
-	return (qx,qy,qz,qw)
+class IMU_Node():
+    
+    def __init__(self):
+        
+        rospy.init_node("IMU_Node")
+        
+        try:
+            port = rospy.get_param('/port', '/dev/ttyUSB0')
+        except rospy.ROSException:
+            rospy.logerr("Parameter '/port' not defined. Using default port '/dev/ttyUSB0'.")
+            port = '/dev/ttyUSB0'
 
-def convert_to_quaternion(r,p,y):
-    qx, qy, qz, qw = toquat(r, p, y)
-    return EulerToQuaternionResponse(qx, qy, qz, qw)
+        pub = rospy.Publisher("/imu", imu_msg, queue_size=100)
+        
+        self.imu_data = imu_msg()
+        self.imu_data.Header.frame_id = "IMU1_Frame"
+        self.imu_data.IMU.header.frame_id = "IMU1_Frame"
+        self.imu_data.MagField.header.frame_id = "IMU1_Frame"
 
-def convert_to_quaternion_server():
-    #rospy.init_node('convert_to_quaternion_server')
-    s = rospy.Service('convert_to_quaternion', EulerToQuaternion, convert_to_quaternion)
-    #print("Ready to add two ints.")
-    #rospy.spin()
+        self.euler_angles = Vector3()
+
+        try:
+            ser = self.open_serial_port(port)
+        except rospy.ROSInterruptException:
+            pass  # Handle ROS interrupt exception if needed
+        except Exception as e:
+            rospy.logerr(f"An unexpected error occurred: {e}")
+            exit()
+        
+        while not rospy.is_shutdown():
+
+            raw_data = ser.readline().decode()
+            self.imu_data.raw = raw_data
+
+            raw_data = raw_data.split()
+
+            self.imu_data.Header.stamp = rospy.Time.now()
+            self.imu_data.IMU.header.stamp = self.imu_data.Header.stamp
+            self.imu_data.MagField.header.stamp = self.imu_data.Header.stamp
+
+            try:
+                self.euler_angles.x = float(raw_data[3]) * math.pi / 180
+                self.euler_angles.y = float(raw_data[2]) * math.pi / 180
+                self.euler_angles.z = float(raw_data[1]) * math.pi / 180
+                
+                self.imu_data.IMU.orientation = self.get_quaternions()
+
+                self.imu_data.IMU.linear_acceleration.x = float(raw_data[7])
+                self.imu_data.IMU.linear_acceleration.y = float(raw_data[8])
+                self.imu_data.IMU.linear_acceleration.z = float(raw_data[9])
+
+                self.imu_data.MagField.magnetic_field.x = float(raw_data[4])
+                self.imu_data.MagField.magnetic_field.y = float(raw_data[5])
+                self.imu_data.MagField.magnetic_field.z = float(raw_data[6])
+
+                self.imu_data.IMU.angular_velocity.x = float(raw_data[10])
+                self.imu_data.IMU.angular_velocity.y = float(raw_data[11])
+                self.imu_data.IMU.angular_velocity.z = float(raw_data[12][:-5])
+
+            except Exception as e:
+                rospy.logerr(f"Error processing IMU data: {e}")
+                continue
+
+            pub.publish(self.imu_data)
+        
+        ser.close()
+
+    def open_serial_port(self, port, timeout=60):
+        start_time = rospy.Time.now()
+
+        while rospy.Time.now() - start_time < rospy.Duration(timeout) and not rospy.is_shutdown():
+            try:
+                ser = serial.Serial(port, 115200, timeout=3)
+                rospy.loginfo(f"Serial port {port} opened successfully.")
+                return ser
+            except serial.SerialException:
+                rospy.logwarn(f"Error! NO SIGNAL")
+                rospy.sleep(1.0)
+
+        rospy.logerr(f"Timeout ({timeout} seconds) reached. Unable to open {port}. Exiting.")
+        rospy.signal_shutdown("Serial port timeout")
+        exit()
+
+    def get_quaternions(self):
+        rospy.wait_for_service("convert_to_quaternion")
+
+        try:
+            get_quat = rospy.ServiceProxy("convert_to_quaternion", convert_to_quaternion)
+            quat = get_quat(self.euler_angles)
+            return quat.quaternion
+        except rospy.ServiceException as e:
+            rospy.logerr(f"Service call failed: {e}")
 
 if __name__ == '__main__':
-
-    # prtv=rospy.get_param('imu_driver/port', '/dev/ttyUSB0')
-    rospy.init_node('imu_driver') 
-   
-    pub = rospy.Publisher('imu', imu_msg, queue_size=5)    
-    rospy.loginfo("Publishing IMU and MagField Data")
-
-    #Initializing the conversion server
-    convert_to_quaternion_server() 
-    # rospy.wait_for_service('convert_to_quaternion')
-    # eulertoquat = rospy.ServiceProxy('convert_to_quaternion', EulerToQuaternion)
-
-    if len(sys.argv) > 0:
-        rospy.loginfo(sys.argv[0])
-        port_param = rospy.get_param('imu_driver/port', sys.argv[1])
-    else:
-        port_param = rospy.get_param('imu_driver/port', '/dev/ttyUSB0')
-
-    # port_param = rospy.get_param('imu_driver/port', '/dev/ttyUSB0')
-    port = serial.Serial(port_param, 115200, timeout=3.)
-    port.write(b"VNWRG,07,40*XX")
-    
-    imu_parsed = imu_msg()
-    imu_parsed.Header.frame_id="IMU1_FRAME"
-    imu_parsed.Header.seq=0
-    imu_parsed.IMU.header.frame_id="IMU1_FRAME"
-    imu_parsed.MagField.header.frame_id="IMU1_FRAME"
-    
-    
-    sleep_time = 0.25;
-   
-    try:
-        while not rospy.is_shutdown():
-            line = port.readline().decode().strip()
-
-            if line.split(',')[0] == "$VNYMR":                
-                try: 
-                    yprmag = line.split(',')
-                    imu_parsed.Header.stamp=rospy.Time.now()
-                    imu_parsed.IMU.header.stamp=rospy.Time.now()
-                    imu_parsed.MagField.header.stamp=rospy.Time.now()
-                    imu_parsed.IMU.header.seq=imu_parsed.Header.seq
-                    imu_parsed.MagField.header.seq=imu_parsed.Header.seq
-
-                    # rospy.wait_for_service('convert_to_quaternion')
-                    # quaternions = rospy.ServiceProxy('convert_to_quaternion', AddTwoInts)
-
-                    req = EulerToQuaternionRequest()
-                    req.roll = float(yprmag[3])
-                    req.pitch = float(yprmag[2])
-                    req.yaw = float(yprmag[1])
-
-                    response = convert_to_quaternion(req)
-                    # (qqx,qqy,qqz,qqw)=convert_to_quaternion(float(yprmag[3]),float(yprmag[2]),float(yprmag[1]))
-                    imu_parsed.IMU.orientation.x=response.x
-                    imu_parsed.IMU.orientation.y=response.y
-                    imu_parsed.IMU.orientation.z=response.z
-                    imu_parsed.IMU.orientation.w=response.w
-
-                    imu_parsed.IMU.linear_acceleration.x=float(yprmag[7])
-                    imu_parsed.IMU.linear_acceleration.y=float(yprmag[8])
-                    imu_parsed.IMU.linear_acceleration.z=float(yprmag[9])
-
-                    imu_parsed.IMU.angular_velocity.x=float(yprmag[10])
-                    imu_parsed.IMU.angular_velocity.y=float(yprmag[11])
-                    imu_parsed.IMU.angular_velocity.z=float(yprmag[12][0:-3])
-
-                    imu_parsed.MagField.magnetic_field.x=float(yprmag[4])
-                    imu_parsed.MagField.magnetic_field.y=float(yprmag[5])
-                    imu_parsed.MagField.magnetic_field.z=float(yprmag[6])
-
-                    imu_parsed.raw=line
-
-                except: 
-                    rospy.logwarn("Data exception: "+line)
-                    continue
-            pub.publish(imu_parsed)
-        rospy.sleep(sleep_time)
-            
-    except rospy.ROSInterruptException:
-        port.close()
-    
-    except serial.serialutil.SerialException:
-        rospy.loginfo("Shutting down imu driver node...")
+    IMU_Node()
